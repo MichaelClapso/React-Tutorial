cc21324f016ec01c7fbd9aac013fbfb3e597ae49
MichaelClapso <michael.clapso@yahoo.com>
2021-08-27 15:32:02 -0400

    state is stored in the Board component

--- src/index.js
+++ src/index.js
@@ -29,9 +29,15 @@ class Board extends React.Component {
       squares: Array(9).fill(null),
     };
   }
+
+  handleClick(i) {
+    const squares = this.state.squares.slice();
+    squares[i] = 'X';
+    this.setState({ squares: squares });
+  }
   renderSquare(i) {
     return (
-      <square
+      <Square
         value={this.state.squares[i]}
         onClick={() => this.handleCLick(i)}
       />
