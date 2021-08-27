0191f5ca23a1fe1ebd23cf637465c3c8a866abbe
MichaelClapso <michael.clapso@yahoo.com>
2021-08-27 10:37:56 -0400

    added board constructor

--- src/index.js
+++ src/index.js
@@ -14,7 +14,8 @@ class Square extends React.Component {
   render() {
     return (
       <button className="square"
-        onClick={() => this.setState({ value: 'X' })}>
+        onClick={() => this.setState({ value: 'X' })}
+      >
         {this.state.value}
       </button>
     );
@@ -22,8 +23,14 @@ class Square extends React.Component {
 }
 
 class Board extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      squares: Array(9).fill(null),
+    };
+  }
   renderSquare(i) {
-    return <Square value={i} />;
+    return <Square value={this.state.squares[i]} />;
   }
 
   render() {
